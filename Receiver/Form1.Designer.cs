38bb9b9baa07484d827373adcdd51aeda4bbdafc
bluesnooker123 <dev333perfect@gmail.com>
2023-01-19 22:50:11 +0800

    initial commit

---
+++ Receiver/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Receiver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_signature = new System.Windows.Forms.TextBox();
+            this.button_verifySignature = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button_open = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_message.Location = new System.Drawing.Point(58, 62);
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(385, 26);
+            this.textBox_message.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please type message from the sender";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Please type signature from the sender";
+            // 
+            // textBox_signature
+            // 
+            this.textBox_signature.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_signature.Location = new System.Drawing.Point(58, 150);
+            this.textBox_signature.Multiline = true;
+            this.textBox_signature.Name = "textBox_signature";
+            this.textBox_signature.Size = new System.Drawing.Size(385, 155);
+            this.textBox_signature.TabIndex = 2;
+            // 
+            // button_verifySignature
+            // 
+            this.button_verifySignature.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_verifySignature.Location = new System.Drawing.Point(130, 376);
+            this.button_verifySignature.Name = "button_verifySignature";
+            this.button_verifySignature.Size = new System.Drawing.Size(227, 56);
+            this.button_verifySignature.TabIndex = 4;
+            this.button_verifySignature.Text = "Verify Signature";
+            this.button_verifySignature.UseVisualStyleBackColor = true;
+            this.button_verifySignature.Click += new System.EventHandler(this.button_verifySignature_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Pem Files (*.pem) | *.pem";
+            // 
+            // button_open
+            // 
+            this.button_open.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_open.Location = new System.Drawing.Point(296, 319);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(111, 37);
+            this.button_open.TabIndex = 6;
+            this.button_open.Text = "Open";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(54, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Please choose public key file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_verifySignature);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_signature);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_message);
+            this.Name = "Form1";
+            this.Text = "Receiver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_signature;
+        private System.Windows.Forms.Button button_verifySignature;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Receiver/Form1.cs
@@ -0,0 +1,75 @@
+﻿using CSharp_easy_RSA_PEM;
+using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace Receiver
+{
+    public partial class Form1 : Form
+    {
+        string FilePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = openFileDialog1.FileName;
+            }
+
+        }
+
+        private void button_verifySignature_Click(object sender, EventArgs e)
+        {
+            if (textBox_message.TextLength == 0)
+            {
+                MessageBox.Show("Please type message!");
+                return;
+            }
+            if (textBox_signature.TextLength == 0)
+            {
+                MessageBox.Show("Please type signature!");
+                return;
+            }
+            if (FilePath == "")
+            {
+                MessageBox.Show("Please choose public key file!");
+                return;
+            }
+            try
+            {
+                string loadedRSA = File.ReadAllText(FilePath);
+                RSACryptoServiceProvider publicRSAkey = Crypto.DecodeRsaPublicKey(loadedRSA);
+
+                SHA1Managed sha1 = new SHA1Managed();
+                string importantMessage = textBox_message.Text;
+                byte[] importantMessageBytes = Encoding.UTF8.GetBytes(importantMessage);
+                byte[] hashedMessage = sha1.ComputeHash(importantMessageBytes);
+
+                byte[] signatureBytes = Convert.FromBase64String(textBox_signature.Text);
+
+                bool isSignatureOkay = publicRSAkey.VerifyHash(hashedMessage, CryptoConfig.MapNameToOID("SHA1"), signatureBytes);
+
+                if (isSignatureOkay)
+                {
+                    MessageBox.Show("Signature is okay!");
+                }
+                else
+                {
+                    MessageBox.Show("Signature is wrong!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+
+        }
+    }
+}
